687524aeeea31d619903639ac786b2f7694f865d
Vijay Kumar Modak <vijaymodak9945@gmail.com>
2025-07-20 08:46:02 +0530

    Update constant.js

--- frontend/utils/constant.js
+++ frontend/utils/constant.js
@@ -1,2 +1,2 @@
-export const USER_END_POINT = "http://localhost:8080/api/v1/user";
-export const MESSAGE_END_POINT = "http://localhost:8080/api/v1/message";
+export const USER_END_POINT = "https://mern-chat-backend-6gnb.onrender.com/api/v1/user";
+export const MESSAGE_END_POINT = "https://mern-chat-backend-6gnb.onrender.com/api/v1/message";
